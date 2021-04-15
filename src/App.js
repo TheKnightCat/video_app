5f3f808f53ce29b8d3d1bfda500af6ff6fe4f1b4
TheKnightCat <60698813+TheKnightCat@users.noreply.github.com>
2021-04-15 22:45:04 +0300

    Добавлено отображение видео

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import React from "react";
+import ReactPlayer from "react-player";
+
+import "./App.css";
+import vid from "./SnowyTrees.mp4";
 
 function App() {
   return (
     <div className="App">
-
+      <ReactPlayer url={"https://youtu.be/WjoplqS1u18"} volume={false} />
+      <video
+        src={vid}
+        autoPlay
+        controls
+        height="300px"
+        width="400px"
+        datatype="video/mp4"
+      />
     </div>
   );
 }
